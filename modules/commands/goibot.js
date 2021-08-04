7a42abe56397d710761586be2c59170dae10b2b4
demngayyeu <nguyenmanhict@gmail.com>
2021-08-04 06:52:04 -0700

    first commit

---
+++ modules/commands/goibot.js
@@ -0,0 +1,55 @@
+const fs = global.nodemodule["fs-extra"];
+module.exports.config = {
+  name: "goibot",
+  version: "1.0.1",
+  hasPermssion: 0,
+  credits: "manhIT",
+  description: "goibot",
+  commandCategory: "Noprefix",
+  usages: "noprefix",
+  cooldowns: 5,
+};
+module.exports.handleEvent = function({ api, event }) {
+  var { threadID, messageID } = event;
+  var tl = ["chào bạn tôi là bot Mạnh", "bạn gọi tôi có việc gì?", "tôi yêu bạn vai lon", "Yêu em <3", "Hi, chào con vợ bé:3", "Vợ gọi có việc gì không?", "Sử dụng callad để liên lạc với admin!"];
+  var rand = tl[Math.floor(Math.random() * tl.length)]
+
+  if ((event.body.toLowerCase() == "bot ngu")) {
+    return api.sendMessage("ngu con cac, bố mày out cho zừa lòng mày, được chưa :))", threadID, () =>
+      api.removeUserFromGroup(api.getCurrentUserID(), threadID));
+  };
+
+  if ((event.body.toLowerCase() == "bot lồn") || (event.body.toLowerCase() == "bot lon")) {
+    return api.sendMessage("dm con ml rác rưỡi", threadID);
+  };
+
+  if ((event.body.toLowerCase() == "bot óc chó") || (event.body.toLowerCase() == "bot oc")) {
+    return api.sendMessage("óc chó mới chửi bot, cmm súc vật học", threadID);
+  };
+
+  if ((event.body.toLowerCase() == "bot ơi") || (event.body.toLowerCase() == "bot oi")) {
+    return api.sendMessage("Dạ, có em đây, yêu em không mà gọi <3. hmm...", threadID);
+  };
+
+  if ((event.body.toLowerCase() == "yêu bot") || (event.body.toLowerCase() == "yeu bot")) {
+    return api.sendMessage("Hmm... Bot ko biết yêu, yêu admin bot kia kìa :))", threadID);
+  };
+
+   if ((event.body.toLowerCase() == "yêu anh") || (event.body.toLowerCase() == "yeu anh")) {
+    return api.sendMessage("Anh cũng yêu em <3", threadID);
+  };
+
+  if ((event.body.toLowerCase() == "bot có yêu em không") || (event.body.toLowerCase() == "bot yeu em khong")) {
+    return api.sendMessage("Hi, Bot yêu em hơn cả ny em cơ, yêu bot đi <3", threadID);
+  };
+
+  if (event.body.indexOf("bot") == 0 || (event.body.indexOf("Bot") == 0)) {
+    var msg = {
+      body: rand
+    }
+    return api.sendMessage(msg, threadID, messageID);
+  };
+
+}
+
+module.exports.run = function({ api, event, client, __GLOBAL }) { }
