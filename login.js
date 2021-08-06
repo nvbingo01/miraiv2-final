fba8680e81d32cf3d2116a64f16e5fb3474eae4c
demngayyeu <nguyenmanhict@gmail.com>
2021-08-06 13:05:36 -0700

    first commit

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 const fs = require("fs-extra");
-const login = require("@miraipr0ject/fca-unofficial");
+const login = require("@maihuybao/fca-unofficial");
 const readline = require("readline");
 const totp = require("totp-generator");
 
@@ -9,14 +9,14 @@ if (argv.length !== 0) configPath = argv[0];
 else configPath = "./config.json";
 
 var rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
 
 const option = {
-	logLevel: "silent",
-	forceLogin: true,
-	userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36"
+    logLevel: "silent",
+    forceLogin: true,
+    userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36"
 };
 
 const config = require(`./${configPath}`);
@@ -25,26 +25,26 @@ let password = config.PASSWORD;
 let otpkey = config.OTPKEY.replace(/\s+/g, '').toLowerCase();
 
 login({ email, password }, option, (err, api) => {
-	if (err) {
-		switch (err.error) {
-			case "login-approval":
-				if (otpkey) err.continue(totp(otpkey));
-				else {
-					console.log("Nhập mã xác minh 2 lớp:");
-					rl.on("line", line => {
-						err.continue(line);
-						rl.close();
-					});
-				}
-				break;
-			default:
-			console.error(err);
-			process.exit(1);
-		}
-		return;
-	}
-	const json = JSON.stringify(api.getAppState());
-	fs.writeFileSync(`./${config.APPSTATEPATH}`, json);
-	console.log("Đã ghi xong appstate!");
-	process.exit(0);
+    if (err) {
+        switch (err.error) {
+            case "login-approval":
+                if (otpkey) err.continue(totp(otpkey));
+                else {
+                    console.log("Nhập mã xác minh 2 lớp:");
+                    rl.on("line", line => {
+                        err.continue(line);
+                        rl.close();
+                    });
+                }
+                break;
+            default:
+                console.error(err);
+                process.exit(1);
+        }
+        return;
+    }
+    const json = JSON.stringify(api.getAppState());
+    fs.writeFileSync(`./${config.APPSTATEPATH}`, json);
+    console.log("Đã ghi xong appstate!");
+    process.exit(0);
 });
